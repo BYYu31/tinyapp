a7f22431443dc781130624e7524c4b32c5a609d3
Bo Yang Yu <boyangyu93@gmail.com>
2023-06-23 23:39:13 -0400

    create path and template when creating new urls

--- express_server.js
+++ express_server.js
@@ -3,27 +3,55 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 
+// generate random 6 digit code
+function generateRandomString() {
+  const alphanumeric = 'abcdefghijklmnopkrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTYUVWXYZ';
+  let result = '';
+  
+  for (let i = 0; i < 6; i ++) {
+    result += alphanumeric[Math.floor(Math.random() * alphanumeric.length)];
+  }
+  return result;
+}
+
+// database
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// home path
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+// urls path
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
 
+// getting new urls
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+// creating new urls
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
+
+// getting urls with specific id
 app.get("/urls/:id", (req,res) => {
   let id = req.params.id;
   const templateVars = { id: id, longURL: urlDatabase[id] }
   res.render("urls_show", templateVars);
 })
 
+// getting urls.json file
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
